0cecd34f1e3ea933987b840f951a2b81a1957d51
arfaqkhna96 <105111298+arfaqkhna96@users.noreply.github.com>
2025-01-13 12:25:35 +0530

    Update kill.bat

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,1 +1,11 @@
+@echo off
+echo The following command terminates the "npm start" process using its PID
+echo (written to ".pidfile"), all of which were conducted when "deliver.bat"
+echo was executed.
+
+REM Enable command echoing
+echo on
+
+REM Terminate the process using the PID from ".pidfile"
+for /f "tokens=* usebackq" %%x in (".pidfile") do taskkill /PID %%x /F
 
